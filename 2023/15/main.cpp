47b6ea70ae4988cf695bf8eb3cdad71ddba196bf
0x35c <unai64@orange.fr>
2023-12-15 22:31:44 +0100

    day 15 p2 done

--- 2023/15/main.cpp
+++ 2023/15/main.cpp
@@ -3,7 +3,18 @@
 #include <iostream>
 #include <vector>
 
-static void split(const std::string& str, char sep, std::vector<std::string> &dest){
+typedef struct {
+	std::string label;
+	int focal;
+} Lens;
+
+static void split(const std::string& str, char sep, std::vector<std::string> &dest);
+static void find_lens(const std::vector<std::vector<Lens>> &boxes, const std::string &label);
+static void parse_lenses(std::vector<std::string> &lenses, std::vector<std::vector<Lens>> &boxes);
+static int hash_string(const std::string &str);
+
+static void split(const std::string& str, char sep, std::vector<std::string> &dest)
+{
 	std::string token;
 
 	for (auto c : str) {
@@ -17,7 +28,49 @@ static void split(const std::string& str, char sep, std::vector<std::string> &de
 	dest.push_back(token);
 }
 
-static int hash_string(const std::string &str) {
+static int find_lens(const std::vector<Lens> &box, const std::string &label)
+{
+	for (auto i = 0; i < box.size(); i++) {
+		if (label == box[i].label)
+			return (i);
+	}
+	return (-1);
+}
+
+static void parse_lenses(std::vector<std::string> &lenses, std::vector<std::vector<Lens>> &boxes)
+{
+	for (auto i = 0; i < lenses.size(); i++) {
+		auto lens = lenses[i];
+		std::string label;
+		auto k = 0;
+		while (auto c = lens[k]) {
+			if (c == '=' || c == '-')
+				break;
+			label += c;
+			k++;
+		}
+		int box_pos = hash_string(label);
+		std::vector<Lens> &box = boxes[box_pos];
+
+		if (lens[k] == '=') {
+			int pos = find_lens(box, label);
+			if (pos != -1) {
+				box[pos].label = label;
+				box[pos].focal = lens[k + 1] - '0';
+			} else {
+				Lens lens_node = { label, lens[k + 1] - '0'};
+				box.push_back(lens_node);
+			}
+		} else {
+			int pos = find_lens(box, label);
+			if (pos != -1)
+				box.erase(box.begin() + pos);
+		}
+	}
+}
+
+static int hash_string(const std::string &str)
+{
 	int value = 0;
 	for (auto c : str) {
 		value += c;
@@ -27,11 +80,13 @@ static int hash_string(const std::string &str) {
 	return (value);
 }
 
-int main(void) {
+int main(void)
+{
 	int result = 0;
 	char *input = NULL;
 	size_t size;
 	std::vector<std::string> strings;
+	std::vector<std::vector<Lens>> boxes;
 
 	while (1) {
 		int rv = getline(&input, &size, stdin);
@@ -41,12 +96,22 @@ int main(void) {
 	}
 	free(input);
 
-	for (auto e : strings) {
-		if (e.empty())
-			continue;
-		result += hash_string(e);
-		printf("%s => %d\n", e.c_str(), hash_string(e));
+	for (auto i = 0; i < 256; i++)
+		boxes.push_back({});
+
+	parse_lenses(strings, boxes);
+	for (auto i = 0; i < boxes.size(); i++) {
+		for (auto k = 0; k < boxes[i].size(); k++) {
+			result += (i + 1) * (k + 1) * boxes[i][k].focal;
+		}
 	}
+	//
+	// for (auto el : strings) {
+	// 	if (el.empty())
+	// 		continue;
+	// 	result += hash_string(el);
+	// 	printf("%s => %d\n", el.c_str(), hash_string(el));
+	// }
 
 	printf("%d\n", result);
 
