824a356ab4b9d3565e94f3ccc9eef5da4960a1f3
0x35c <unai64@orange.fr>
2024-12-05 13:47:38 +0100

    d05 done uwu

---
+++ 2024/05/part1.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+static std::vector<int> split_to_int(const std::string &s, char sep)
+{
+	std::vector<int> array;
+	std::string e;
+
+	for (auto i = 0; s[i]; i++) {
+		if (s[i] == sep) {
+			if (!e.empty()) {
+				array.push_back(atoi(e.c_str()));
+				e.clear();
+			}
+			continue;
+		}
+		e += s[i];
+	}
+	if (!e.empty())
+		array.push_back(atoi(e.c_str()));
+	return array;
+}
+
+int main(void)
+{
+	int result = 0;
+	std::string input;
+	std::vector<std::pair<int, int>> rules;
+	std::vector<std::vector<int>> pages;
+
+	while (std::getline(std::cin, input)) {
+		if (input.empty())
+			break;
+		int a = atoi(input.c_str());
+		input.erase(0, 3);
+		int b = atoi(input.c_str());
+		rules.push_back({a, b});
+	}
+	while (std::getline(std::cin, input))
+		pages.push_back(split_to_int(input, ','));
+
+	for (auto line : pages) {
+		bool uwu = true;
+		for (auto rule : rules) {
+			auto a =
+			    std::find(line.begin(), line.end(), rule.first);
+			auto b =
+			    std::find(line.begin(), line.end(), rule.second);
+			if (a == line.end() || b == line.end())
+				continue;
+			if (a < b)
+				continue;
+			uwu = false;
+		}
+		if (uwu)
+			result += line[line.size() / 2];
+	}
+
+	printf("%d\n", result);
+
+	return EXIT_SUCCESS;
+}
