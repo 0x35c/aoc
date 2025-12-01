5e239679c609a6eee9997003acd93380614bec8b
0x35c <>
2025-12-01 10:42:15 +0100

    day01 part2 done

---
+++ 2025/01/part2.cpp
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string>
+
+int main(void)
+{
+	long result = 0;
+	char *input = NULL;
+	size_t size;
+	int dial = 50;
+	bool has_been_zeroed = false;
+
+	int line = 0;
+	while (1) {
+		int rv = getline(&input, &size, stdin);
+		line++;
+		if (rv < 1)
+			break;
+		int rotation = atoi(&input[1]);
+		result += rotation / 100;
+		rotation %= 100;
+		if (input[0] == 'L') {
+			if (dial == 0)
+				has_been_zeroed = true;
+			dial -= rotation;
+			if (dial == 0)
+				result++;
+			else if (dial < 0) {
+				if (has_been_zeroed)
+					has_been_zeroed = false;
+				else
+					result++;
+				dial += 100;
+			}
+		} else {
+			dial += rotation;
+			if (dial == 100)
+				result++;
+			else
+				result += dial / 100;
+			dial %= 100;
+		}
+	}
+	free(input);
+
+	printf("%ld\n", result);
+
+	return EXIT_SUCCESS;
+}
