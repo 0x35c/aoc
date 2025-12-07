7fb1696b30adc3eefd7e6962fb98ae2215a5bbc4
0x35c <>
2025-12-07 18:06:39 +0100

    d07p1 done

---
+++ 2025/07/part1.cpp
@@ -0,0 +1,43 @@
+#include "../utils.hpp"
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(void)
+{
+	long result = 0;
+	char *input = NULL;
+	size_t size;
+	std::vector<std::string> map;
+
+	while (1) {
+		int rv = getline(&input, &size, stdin);
+		if (rv < 1)
+			break;
+		if (input[rv - 1] == '\n') {
+			input[rv - 1] = 0;
+		}
+		map.push_back(input);
+	}
+	free(input);
+
+	for (auto y = 0; y < map.size() - 1; y++) {
+		for (auto x = 0; x < map[0].size(); x++) {
+			if (map[y][x] == '.' || map[y][x] == '^')
+				continue;
+			if (map[y + 1][x] == '.') {
+				map[y + 1][x] = '|';
+				continue;
+			} else if (map[y + 1][x] == '^') {
+				result++;
+				map[y + 1][x - 1] = '|';
+				map[y + 1][x + 1] = '|';
+			}
+		}
+	}
+
+	printf("%ld\n", result);
+
+	return EXIT_SUCCESS;
+}
